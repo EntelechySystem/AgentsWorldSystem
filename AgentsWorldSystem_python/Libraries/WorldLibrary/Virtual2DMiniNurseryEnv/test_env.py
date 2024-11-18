a71c30065d340266f627bdea9d2cb978caffda76
Yee Cube <EthanSystem@outlook.com>
2024-11-18 17:59:40 +0800

    add 开发不依赖 PettingZoo 的环境

---
+++ AgentsWorldSystem_python/Libraries/WorldLibrary/Virtual2DMiniNurseryEnv/test_env.py
@@ -0,0 +1,16 @@
+import pygame
+
+from Libraries.WorldLibrary.Virtual2DMiniNurseryEnv_pettingzoo_mpe.world_environment import env, parallel_env, raw_env
+
+my_env = parallel_env(render_mode="human")
+observations, infos = my_env.reset()
+
+while my_env.agents:
+    # this is where you would insert your policy
+    actions = {agent: my_env.action_space(agent).sample() for agent in my_env.agents}
+
+    observations, rewards, terminations, truncations, infos = my_env.step(actions)
+
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        pygame.quit()
